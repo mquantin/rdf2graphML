9827d9aae5ad9cbbf77b8b38beee9b8068693e80
mquantin <matthieu@tinta.fr>
2023-07-03 13:34:46 +0200

    label attr = shortest str from a list of label candidates

--- rdfToGraphML.py
+++ rdfToGraphML.py
@@ -3,7 +3,7 @@ import rdflib
 import networkx as nx
 from getURIvalue import getValue, updateURIlabelLocalFile
 import re
-
+from collections import defaultdict
 
 ## CONSTANTS ##
 rdf_file_path = "/Users/ls2n/Documents/utils/omekas2rdf/files/items.ttl"
@@ -28,17 +28,20 @@ def moreNodeAttributes():
             nodeObj['label'] = getValue(n, 'skos/core#prefLabel|geonames.org/ontology#alternateName|geonames.org/ontology#name')
 
 def feedGraph():
+    labelCandidates = defaultdict(list)
     for s, p, o in rdf_graph.triples((None, None, None)):
         # The .toPython() method converts rdflib objects into objects
         #  that any Python module can understand (e.g. str, int, float).
 
         predic = prefixForURI(p)
-
+        print(predic)
         #following cases won't create a new node for the object but only a attribute to the actual nodes
         # this simplifies the graph
-        if predic == 'rdfs:label':
-            graph.add_node(s.toPython(), label=o.toPython())
-        elif predic == 'rdf:type':
+        # create a label attribute with a short content if available
+        accepted_labels = {'bibo:shortTitle', 'skos:prefLabel', 'rdfs:label', 'skos:altLabel'}
+        if predic in accepted_labels:
+            labelCandidates[s].append(o.toPython())
+        if predic == 'rdf:type':
             graph.add_node(s.toPython(), type=prefixForURI(o))#with only prefix not whole URI
         elif not re.match('https?://', o.toPython()):
             #case other object not an other node URI but a simple string or integer
@@ -47,7 +50,10 @@ def feedGraph():
         #case edge to another (entity)
         else:
             graph.add_edge(s.toPython(), o.toPython(), predicate=predic)
+    for sujet, labels in labelCandidates.items():
+        graph.add_node(sujet.toPython(), label=min(labels, key=len))
     print(f'Added {graph.order()} nodes and {graph.size()} edges to the graph')
+
 
 feedGraph()
 moreNodeAttributes()
